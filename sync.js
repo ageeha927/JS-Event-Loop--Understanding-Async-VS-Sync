01ea13144637b25c78f2d5abe90fe32515e2cc01
hey.its.a <141285328+ageeha927@users.noreply.github.com>
2024-08-29 13:54:52 -0700

    .

--- sync.js
+++ sync.js
@@ -25,5 +25,15 @@ console.log(fs.readFileSync('input.txt', 'utf-8')) //sync
 //the sync appeared before the async 
 
 //Problem #4
+console.log(fs.readFileSync('data.txt', 'utf-8')) //sync
+setTimeout(() => { //async
+    console.log('Starting async operation..')
+    const file = fs.readFileSync('data.txt', 'utf-8')
+    console.log(file)
+}, "2000");
+console.log("Script complete")
+
+//Problem 5
+console.log("Count down started")
 
 
